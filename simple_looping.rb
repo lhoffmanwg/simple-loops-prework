32913cf8aafb6f8af7dac007f7011dc201e9f771
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-31 03:08:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,14 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  counter = 0
+  counter = 1 
   loop do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
-    puts phrase
-     
     if counter > number_of_times 
       break
     end
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     counter += 1
     
 end
