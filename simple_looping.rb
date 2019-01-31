8ba34f2ead542414596546fcc3598a139dd68b9a
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-31 03:00:40 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
-    counter += 1 
+     
     if counter > number_of_times 
       break
     end
