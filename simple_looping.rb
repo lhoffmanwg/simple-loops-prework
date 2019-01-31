aecb2d2919f89c6eedb2a4b8d70a39edb1ea8ee9
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-31 03:09:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  counter = 0  
+  counter = 1   
   loop do
     if counter > number_of_times 
       break
